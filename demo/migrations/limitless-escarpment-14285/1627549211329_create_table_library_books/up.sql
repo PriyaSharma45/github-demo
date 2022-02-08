32a04f921278e36f5504c5e0c4a4348e7035d74c
Priya Sharma <priaasharma4@gmail.com>
2022-02-08 18:55:34 +0530

    add a column title

--- demo/migrations/limitless-escarpment-14285/1627549211329_create_table_library_books/up.sql
+++ demo/migrations/limitless-escarpment-14285/1627549211329_create_table_library_books/up.sql
@@ -1,3 +1,5 @@
 CREATE TABLE "library"."books" ("id" serial NOT NULL, PRIMARY KEY ("id") );
 
 CREATE TABLE "library"."test" ("id" serial NOT NULL, PRIMARY KEY ("id") );
+
+ALTER TABLE "library"."books" ADD COLUMN "title" text NOT NULL;
