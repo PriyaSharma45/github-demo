5fad25f2250dec7bbce0822b924c333edd31588f
Priya Sharma <priaasharma4@gmail.com>
2022-03-23 12:38:37 +0530

    insert value to test

--- demo/migrations/limitless-escarpment-14285/1627549211329_create_table_library_books/up.sql
+++ demo/migrations/limitless-escarpment-14285/1627549211329_create_table_library_books/up.sql
@@ -2,4 +2,7 @@ CREATE TABLE "library"."books" ("id" serial NOT NULL, PRIMARY KEY ("id") );
 
 CREATE TABLE "library"."test" ("id" serial NOT NULL, PRIMARY KEY ("id") );
 
+INSERT INTO "library"."test" (id) VALUES ("afd289a0-b556-4653-92d1-828d4ce62e15");
+
+
 ALTER TABLE "library"."books" ADD COLUMN "title" text NOT NULL;
